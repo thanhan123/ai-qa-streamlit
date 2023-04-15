14439c7f613441dcee47c055e50de8cafa904a20
Dinh Thanh An <thanhan.uit@gmail.com>
2023-04-15 23:55:39 +0900

    use streamlit secret

--- app.py
+++ app.py
@@ -3,18 +3,14 @@ import pinecone
 from langchain import PromptTemplate
 import openai
 from langchain.llms import OpenAI
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 # Set the title of the Streamlit app
 st.title("Mincolor AI Q&A")
 
 @st.cache_resource
 def getPineconeIndex():
-  PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-  PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
+  PINECONE_API_KEY = st.secrets.PINECONE_API_KEY
+  PINECONE_API_ENV = st.secrets.PINECONE_API_ENV
   pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
     environment=PINECONE_API_ENV  # next to api key in console
@@ -41,7 +37,7 @@ Answer: """
 
 @st.cache_resource
 def getCustomOpenAI():
-  OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+  OPENAI_API_KEY = st.secrets.OPENAI_API_KEY
   return OpenAI(
     model_name="gpt-3.5-turbo",
     openai_api_key=OPENAI_API_KEY
